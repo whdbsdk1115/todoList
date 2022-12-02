a61381ad64c4ab5bb28baf2f2679137b9ec3ce2e
whdbsdk1115 <whdbsdk1115@naver.com>
2022-12-02 16:12:56 +0900

    data class

---
+++ src/main/kotlin/com/example/todo/controller/api/todo/model/http/TodoDto.kt
@@ -0,0 +1,46 @@
+package com.example.todo.controller.api.todo.model.http
+
+import com.example.todo.database.Todo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.AssertTrue
+import javax.validation.constraints.NotBlank
+
+data class TodoDto (
+
+    var index: Int?=null,
+
+    @field:NotBlank
+    var title: String?=null,
+
+    var description: String?=null,
+
+    @field:NotBlank
+    // yyyy-MM-dd HH:mm:ss
+    var schedule: String?=null,
+
+    var createdAt: LocalDateTime?=null,
+    var updatedAt: LocalDateTime?=null
+) {
+    // TODO 이전에 학습했던 custom annotation 으로 변경
+    @AssertTrue(message = "yyyy-MM-dd HH:mm:ss 포맷이 맞지 않습니다.")
+    fun validSchedule():Boolean {
+        return try {
+            LocalDateTime.parse(schedule, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
+
+fun TodoDto.convertTodoDto(todo: Todo): TodoDto {
+    return TodoDto().apply {
+        this.index = todo.index
+        this.title = todo.title
+        this.description = todo.description
+        this.schedule = todo.schedule?.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        this.createdAt = todo.createdAt
+        this.updatedAt = todo.updatedAt
+    }
+}
--- src/main/kotlin/com/example/todo/database/Todo.kt
+++ src/main/kotlin/com/example/todo/database/Todo.kt
@@ -1,6 +1,8 @@
 package com.example.todo.database
 
+import com.example.todo.controller.api.todo.model.http.TodoDto
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 // 칼럼 대신
 data class Todo (
@@ -11,3 +13,15 @@ data class Todo (
     var createdAt: LocalDateTime?=null, // 생성 시간
     var updatedAt: LocalDateTime?=null  // 업데이터 시간
 )
+
+fun Todo.convertTodo(todoDto: TodoDto): Todo {
+    return Todo().apply {
+        this.index = todoDto.index
+        this.title = todoDto.title
+        this.description = todoDto.description
+        this.schedule = LocalDateTime.parse(todoDto.schedule,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        this.createdAt = todoDto.createdAt
+        this.updatedAt = todoDto.updatedAt
+    }
+}
